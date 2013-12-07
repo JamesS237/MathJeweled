882826871f709b462a9c11495e34160754280f0b
Jackson <jaxkr@riseup.net>
2013-12-06 18:30:39 -0700

    added stylesheet

--- app.js
+++ app.js
@@ -5,12 +5,18 @@ function getRandomInt(low, high) {
 	return parseInt(Math.random() * (high - low + 1) + low);
 }
 
-total = 0;
+function test() {
+	total = 0;
 
-for (i = 0; i < 100; i++) {
-	random = getRandomInt(1, 50);
-	total += random;
+	for (i = 0; i < 1000000; i++) {
+		random = getRandomInt(1, 50);
+		total += random;
+	}
+
+	total /= 1000000;
+	console.log(total);
 }
 
-total /= 100;
-console.log(total);
+$(document).ready(function() {
+	test();
+});
