564f2ce9263108e6cfa038391d3fe80fcc0016b3
Jackson <jaxkr@riseup.net>
2013-12-06 20:35:01 -0700

    just stashing some js

--- app.js
+++ app.js
@@ -2,13 +2,29 @@ function getRandomInt(low, high) {
 	return parseInt(Math.random() * (high - low + 1) + low);
 }
 
+function generatePiece(identifier) { //identifier is 1-10
+	if (identifier <= 4) { //generate an operator
+
+	} else {
+
+	} //generate a piece
+}
+function generateBoard() {
+	pieces = [];
+	for (i = 0; i < 100; i++) {
+		
+	}
+}
+
 function startGame() {
 	console.log('wep');
 	if ($('#seed').val() !== '') {
 		Math.seedrandom($('#seed').val());
 		console.log(Math.random());
 	}
-	$('#main-menu').hide()
+	$('#main-menu').hide();
+	params = {};
+	generateBoard(params);
 }
 $(document).ready(function() {
 	var currentUnix = new Date().getTime();
