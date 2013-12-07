3e7f8314d2c9453b0de44ad4a9975b693cefd6c6
Jackson <jaxkr@riseup.net>
2013-12-06 20:50:11 -0700

    some js

--- app.js
+++ app.js
@@ -2,18 +2,31 @@ function getRandomInt(low, high) {
 	return parseInt(Math.random() * (high - low + 1) + low);
 }
 
-function generatePiece(identifier) { //identifier is 1-10
-	if (identifier <= 4) { //generate an operator
-
-	} else {
-
-	} //generate a piece
-}
-function generateBoard() {
-	pieces = [];
-	for (i = 0; i < 100; i++) {
-		
+function generatePiece(id, diff) { //identifier is 1-10
+	if (diff == 1) { //easy
+		if (id <= 4) { //generate an operator
+			if (id == 1) {
+				return '+';
+			} else if (id == 2) {
+				return '-';
+			} else if (id == 3) {
+				return '*'
+			} else if (id == 4) {
+				return '/';
+			}
+		} else { //generate a piece
+			return getRandomInt(1,9);
+		}
 	}
+}
+function generateBoard(params) {
+	pieces = [];
+	if (params.diff == 1) { //easy
+		for (i = 0; i < 100; i++) {
+			pieces.push(generatePiece(getRandomInt(1,10), 1));
+		}
+	}
+	console.log(pieces);
 }
 
 function startGame() {
@@ -23,7 +36,7 @@ function startGame() {
 		console.log(Math.random());
 	}
 	$('#main-menu').hide();
-	params = {};
+	params = {diff: $('#diff').val()};
 	generateBoard(params);
 }
 $(document).ready(function() {
